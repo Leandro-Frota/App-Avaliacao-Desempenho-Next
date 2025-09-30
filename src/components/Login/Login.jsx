0ecab721437749ae08cdaa8d6e2f84325c4de42f
leandro-frota <leandromfrota@gmail.com>
2025-09-30 16:03:58 -0300

    Feat: implement user authentication in login component and update API service

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,67 +1,77 @@
-'use client'
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { authenticateUser } from "@/service/apiService";
 
-export default function Login(){
-    const [email, setEmail] = useState('');
-   const [senha, setSenha] = useState('');
-   const [erro, setErro] = useState('');
-   const router = useRouter();
- 
-   const handleLogin = async (e) => {
-     e.preventDefault();
- 
-     // Simulação de login (substituir por chamada real à API)
-     if (email === 'admin@email.com' && senha === '123456') {
-       router.push('/about'); // Redireciona após login
-     } else {
-       setErro('Credenciais inválidas');
-     }
-   };
- 
-   return (
-     <div className="flex items-center justify-center min-w-full min-h-screen  bg-gray-100">
-       <form
-         onSubmit={handleLogin}
-         className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm"
-       >
-         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
- 
-         {erro && (
-           <p className="text-red-600 text-sm mb-4 text-center">{erro}</p>
-         )}
- 
-         <label className="block mb-2 text-sm font-medium text-gray-700">
-           Email
-         </label>
-         <input
-           type="email"
-           value={email}
-           onChange={(e) => setEmail(e.target.value)}
-           className="w-full p-2 border border-gray-300 rounded-lg mb-4"
-           required
-         />
- 
-         <label className="block mb-2 text-sm font-medium text-gray-700">
-           Senha
-         </label>
-         <input
-           type="password"
-           value={senha}
-           onChange={(e) => setSenha(e.target.value)}
-           className="w-full p-2 border border-gray-300 rounded-lg mb-6"
-           required
-         />
- 
-         <button
-           type="submit"
-           className="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition"
-         >
-           Entrar
-         </button>
-         <button className='cursor-pointer active:to-blue-700 focus:to-blue-700  mt-1 font-light '>Recuperar senha</button>
-       </form>
-       
-     </div>
-   );    
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [erro, setErro] = useState("");
+  const router = useRouter();
+  
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await authenticateUser(email, senha);
+      console.log(response);
+
+      if (response.data?.user) {
+        router.push("/dashboard");
+      } else {
+        setErro("Credenciais inválidas. Tente novamente.");
+      }
+    } catch (error) {
+      setErro("Ocorreu um erro. Tente novamente mais tarde.");
+      return;
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-w-full min-h-screen  bg-gray-100">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm"
+      >
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+
+        {erro && (
+          <p className="text-red-600 text-sm mb-4 text-center">{erro}</p>
+        )}
+
+        <label className="block mb-2 text-sm font-medium text-gray-700">
+          Email
+        </label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-lg mb-4"
+          required
+        />
+
+        <label className="block mb-2 text-sm font-medium text-gray-700">
+          Senha
+        </label>
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-lg mb-6"
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition"
+        >
+          Entrar
+        </button>
+        <button className="cursor-pointer active:to-blue-700 focus:to-blue-700  mt-1 font-light ">
+          Recuperar senha
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/service/apiService.js
+++ src/service/apiService.js
@@ -2,20 +2,27 @@ import { URL_API } from "@/Utils/constants/constants";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: URL_API, // Replace with your actual API URL
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+  baseURL: URL_API, // Replace with your actual API URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export const getEmployees = async () => api.get("/employees")
+export const getEmployees = async () => api.get("/employees");
 
-export const registerEmployee = async (data) => api.post("/employees", data)
+export const registerEmployee = async (data) => api.post("/employees", data);
 
-export const updateDataRegisterEmployee = async (employeeId, data) => api.patch(`/employees/${employeeId}`, data)
+export const updateDataRegisterEmployee = async (employeeId, data) =>
+  api.patch(`/employees/${employeeId}`, data);
 
-export const saveStepData = async (employeeId,step,data) => api.put(`/employees/${employeeId}/steps/${step}`, data)
+export const saveStepData = async (employeeId, step, data) =>
+  api.put(`/employees/${employeeId}/steps/${step}`, data);
 
-export const getStepData = async (employeeId, step) => api.get(`/employees/${employeeId}/step/${step}`)
+export const getStepData = async (employeeId, step) =>
+  api.get(`/employees/${employeeId}/step/${step}`);
 
-export const deleteEmployee = async (employeeId) => api.delete(`/employees/${employeeId}`)
+export const deleteEmployee = async (employeeId) =>
+  api.delete(`/employees/${employeeId}`);
+
+export const authenticateUser = async (email, password) =>
+  await api.post("/auth/login", { email, password });
